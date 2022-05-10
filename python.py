984f95a226c60ea0d9a5a2787e293b9d774a69d2
K4m1ka <98820451+K4m1ka@users.noreply.github.com>
2022-05-10 08:42:20 -0300

    python1

--- python.py
+++ python.py
@@ -1,30 +1,25 @@
-#criar um contador que vê o valor do salario no final do mês
+while True:
+    hora = int(input("Salario por hora: "))
+    horatrabalho = int(input("Quantas horas por dia você trabalha: "))
+    salariodia = hora * horatrabalho
+    print(f"Seu salario diario será R$ {salariodia:,.2f}")
 
+    #semanal
 
+    diasemana = int(input("Quantos dias da semana você trabalha: "))
+    salariosemana = diasemana * salariodia
+    print(f"Seu salario semanal será de aproximadamente R$: {salariosemana:,.2f}")
+    #mensal
 
-#criar a variavel do salario diario
+    salariosemanal = int(input("Quantas semanas por mês você trabalha: "))
+    salariomes = salariosemanal * salariosemana
+    print(f"Seu salario mensal será de aproximadamente R$: {salariomes:,.2f}")
 
-hora = int(input("Salario por hora: "))
-horatrabalho = int(input("Quantas horas por dia você trabalha: "))
-salariodia = hora * horatrabalho
-print(f"Seu salario diario será R$ {salariodia:,.2f}")
+    if salariomes<=3000.00:
+        print("Salario classe baixa")
 
-#semanal
+    elif (salariomes<=7000):
+        print("Salario classe média")
 
-diasemana = int(input("Quantos dias da semana você trabalha: "))
-salariosemana = diasemana * salariodia
-print(f"Seu salario semanal será de aproximadamente R$: {salariosemana:,.2f}")
-#mensal
-
-salariosemanal = int(input("Quantas semanas por mês você trabalha: "))
-salariomes = salariosemanal * salariosemana
-print(f"Seu salario mensal será de aproximadamente R$: {salariomes:,.2f}")
-
-if salariomes<=3000.00:
-    print("Salario classe baixa")
-
-elif (salariomes<=7000):
-    print("Salario classe média")
-
-else:
-    print("Salario Classe Alta")
+    else:
+        print("Salario Classe Alta")
